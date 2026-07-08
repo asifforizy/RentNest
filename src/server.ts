8897f8535ad92e255026ef5cbe7335adfca9901c
Asif Forizy <asifforizy@gmail.com>
2026-07-08 13:46:43 +0600

    prisma connected

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import app from "./app";
 import config from "./config";
 import "dotenv/config";
+import { prisma } from "./lib/prisma";
 
 const PORT = config.port;
 
 async function main() {
   try {
+    await prisma.$connect();
+    console.log("connect to database successfully");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-
+     await prisma.$disconnect();
     process.exit(1);
   }
 }
