362fda9a100300b72fc5b06a55d4d9695c2ee19e
Asif Forizy <asifforizy@gmail.com>
2026-07-14 16:45:56 +0600

    final commit

--- src/modules/paymet/payment.route.ts
+++ src/modules/paymet/payment.route.ts
@@ -10,7 +10,7 @@ const router = Router()
 
 
 router.post("/create",auth(UserRole.TENANT), paymentController.createCheckoutSession)
-router.get("/", auth(UserRole.ADMIN,UserRole.LANDLORD,UserRole.TENANT),paymentController.getMyPayments)
+router.get("/", auth(UserRole.TENANT),paymentController.getMyPayments)
 router.get("/:id",  auth(UserRole.LANDLORD, UserRole.TENANT, UserRole.ADMIN), paymentController.getPaymentById)
 router.post("/confirm", auth(UserRole.TENANT), paymentController.confirmPayment);
 
